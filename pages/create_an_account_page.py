2a25f419352bda7ac9fa12bb1a2fdd1f474e807a
Bartosz Dąbrowski <Bartoszeg02@gmail.com>
2019-10-20 21:41:14 +0200

    Dodanie strony "Create an Account"

--- pages/create_an_account_page.py
+++ pages/create_an_account_page.py
@@ -1,7 +1,17 @@
-from base.page_elements import Element, Button, Input
+from base.page_elements import Element, Button, Input, Checkbox
 
 
 class CreateAnAccountPage:
     def __init__(self, driver):
         Element.driver = driver
 
+        self.sign_up_checkbox = Checkbox(
+            locator="#uniform-newsletter input#newsletter",
+            locatorType="css",
+            description="Checkbox 'Sign up for our newsletter!'"
+        )
+        self.sign_up_checkbox_label = Element(
+            locator="#uniform-newsletter input#newsletter",
+            locatorType="css",
+            description="Etykieta 'Sign up for our newsletter!'"
+        )
