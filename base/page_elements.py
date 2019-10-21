54c5a90db6954740f3fcc78032039c4351ad39be
Bartosz Dąbrowski <Bartoszeg02@gmail.com>
2019-10-21 20:30:56 +0200

    Dodanie selectorów do części pól formularza "create an account" Dodanie nowych page elementów: Checkbox, RadioGroup, Radiobutton, Dropdown

--- base/page_elements.py
+++ base/page_elements.py
@@ -3,7 +3,7 @@ from selenium.webdriver.remote.webelement import WebElement
 
 
 class Element:
-    def __init__(self, locator, locatorType='id', description=''):
+    def __init__(self, locator, locatorType='css', description=''):
         self.locator = locator
         self.locatorType = locatorType
         self.description = description
@@ -36,8 +36,16 @@ class Input(Element):
     def send_keys(self, keys):
         return self.element.send_keys(keys)
 
-
 class Checkbox(Element):
     """Klasa obsługująca checkboxy"""
     def check(self):
         self.element.click()
+
+class Radiobutton(Checkbox):
+    """Klasa obsługująca radiobuttony"""
+
+class RadioGroup(Radiobutton):
+    """Klasa obsługująca grupę radiobuttonów"""
+
+class Dropdown(Element):
+    """Klasa obsługująca dropdowny"""
--- pages/create_an_account_page.py
+++ pages/create_an_account_page.py
@@ -1,17 +1,91 @@
-from base.page_elements import Element, Button, Input, Checkbox
+from base.page_elements import Element, Button, Input, Checkbox, RadioGroup, Radiobutton, Dropdown
 
 
 class CreateAnAccountPage:
     def __init__(self, driver):
         Element.driver = driver
 
+        self.page_heading_label = Element(
+            locator=".page-heading",
+            description="Etykieta 'CREATE AN ACCOUNT'"
+        )
+        self.your_personal_information_label = Element(
+            locator=".page-subheading",
+            description="Etykieta 'YOUR PERSONAL INFORMATION'"
+        )
+        self.title_radiobutton = RadioGroup(
+            locator="clearfix",
+            description="Radio grupa 'Title'"
+        )
+        self.first_name_input = Input(
+            locator="#customer_firstname",
+            description="Input 'First name'"
+        )
+        self.last_name_input = Input(
+            locator="#customer_lastname",
+            description="Input 'Last name'"
+        )
+        self.email_input = Input(
+            locator="#email",
+            description="Input 'Email'"
+        )
+        self.password_input = Input(
+            locator="#passwd",
+            description="Input 'Password '"
+        )
+        self.date_of_birth_days_dropdown = Dropdown(
+            locator="days",
+            description="Dropdown Date of Birth - dni"
+        )
+        self.date_of_birth_months_dropdown = Dropdown(
+            locator="#months",
+            description="Dropdown Date of Birth - miesiące"
+        )
+        self.date_of_birth_years_dropdown = Dropdown(
+            locator="#years",
+            description="Dropdown Date of Birth - lata"
+        )
         self.sign_up_checkbox = Checkbox(
             locator="#uniform-newsletter input#newsletter",
-            locatorType="css",
             description="Checkbox 'Sign up for our newsletter!'"
         )
-        self.sign_up_checkbox_label = Element(
-            locator=".checkbox > label[for='newsletter']",
-            locatorType="css",
-            description="Etykieta 'Sign up for our newsletter!'"
+        self.receive_special_offers_checkbox = Checkbox(
+            locator="#uniform-optin input#optin",
+            description="Checkbox 'Sign up for our newsletter!'"
         )
+        self.your_address_label = Element(
+            locator=".page-subheading",
+            description="Etykieta 'YOUR ADDRESS'"
+        )
+
+
+        # self. = Input(
+        #     locator="",
+        #     locatorType="",
+        #     description="Input ''"
+        # )
+        # self. = Input(
+        #     locator="",
+        #     locatorType="",
+        #     description="Input ''"
+        # )
+        # self. = Input(
+        #     locator="",
+        #     locatorType="",
+        #     description="Input ''"
+        # )
+        #
+        #
+
+
+
+
+
+
+
+
+
+        # self.sign_up_checkbox_label = Element(
+        #     locator=".checkbox > label[for='newsletter']",
+        #     description="Etykieta 'Sign up for our newsletter!'"
+        # )
