db99aa3673486a015cdfdaa51e48789cc11d7ac2
Bartosz Dąbrowski <Bartoszeg02@gmail.com>
2019-11-19 18:37:53 +0100

    Dodanie funkcji 'assertion_message' + drobne poprawki

--- base/assertions.py
+++ base/assertions.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-
-
 DEFAULT_ASSERION_MESSAGES = {
     'assert_element_text_equal' : 'Oczekiwana treść elementu »%s« to »%s« a jest »%s«.',
     'assert_element_value_equal' : 'Oczekiwana wartość elementu »%s« to »%s« a jest »%s«.',
@@ -12,51 +9,33 @@ DEFAULT_ASSERION_MESSAGES = {
 def _default_assertion_message(method_name):
     return DEFAULT_ASSERION_MESSAGES[method_name]
 
-def assertion_message(assertion_name, **kwargs):
-    arguments = {}
-    for key, value in kwargs.items():
-        arguments[key] = value
-    if arguments["msg"] is None:
-        del arguments["msg"]
-        msg = _default_assertion_message(assertion_name) % (*arguments.values(),)
-    else:
-        msg = arguments["msg"]
+def assertion_message(assertion_name, *args, msg=None):
+    if msg is None:
+        msg = _default_assertion_message(assertion_name) % (*args,)
     raise Exception(msg)
-
 
 class SeleniumAssertionBasic:
 
-    def assert_element_text_equal(self, element, excepted_text, msg=None):
+    def assert_element_text_equal(self, element, excepted_value, msg=None):
         actual_text = element.get_text()
-        if actual_text != excepted_text:
-            assertion_message("assert_element_text_equal", element=element, excepted_text=excepted_text, actual_text=actual_text, msg=msg)
-            # if msg is None:
-            #     msg = _default_assertion_message('assert_element_text_equal') % (element, excepted_text, actual_text)
-            # raise Exception(msg)
+        if actual_text != excepted_value:
+            assertion_message("assert_element_text_equal", element, excepted_value, actual_text, msg=msg)
 
     def assert_element_value_equal(self, element, excepted_value, msg=None):
         actual_value = element.get_value()
         if actual_value != excepted_value:
-            if msg is None:
-                msg = _default_assertion_message('assert_element_value_equal') % (element, excepted_value, actual_value)
-            raise Exception(msg)
+            assertion_message("assert_element_value_equal", element, excepted_value, actual_value, msg=msg)
 
     def assert_element_value_contains(self, element, contained_value, msg=None):
         actual_value = element.get_value()
         if contained_value not in actual_value:
-            if msg is None:
-                msg = _default_assertion_message('assert_element_value_contains') % (element, contained_value)
-            raise Exception(msg)
+            assertion_message("assert_element_value_contains", element, contained_value, msg=msg)
 
     def assert_element_value_starts(self, element, excepted_value, msg=None):
         actual_value = element.get_value()
         if actual_value.startswith(excepted_value) == False:
-            if msg is None:
-                msg = _default_assertion_message('assert_element_value_starts') % (element, excepted_value)
-            raise Exception(msg)
-        
+            assertion_message("assert_element_value_starts", element, excepted_value, msg=msg)
+
     def assert_element_is_visible(self, element, msg=None):
         if not element.is_visible():
-            if msg is None:
-                msg = _default_assertion_message('assert_element_is_visible') % (element)
-            raise Exception(msg)
+            assertion_message("assert_element_is_visible", element, msg=msg)
