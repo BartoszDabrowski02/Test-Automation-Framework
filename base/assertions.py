0d9cc503a442ad6b603d5d46daf8e70f43831138
Bartosz Dąbrowski <Bartoszeg02@gmail.com>
2019-11-30 14:38:05 +0100

    Poprawki treści w 'DEFAULT_ASSERION_MESSAGES'

--- base/assertions.py
+++ base/assertions.py
@@ -5,15 +5,15 @@ regex = {
     }
 
 DEFAULT_ASSERION_MESSAGES = {
-    'assert_element_text_equal' : 'Oczekiwana treść elementu {0} to {1} a jest {2}.',
-    'assert_element_value_equal' : 'Oczekiwana wartość elementu {0} to {1} a jest {2}.',
-    'assert_element_value_contains' : 'Wartość elementu {0}« nie zawiera {1}.',
-    'assert_element_value_starts' : 'Wartość elementu {0} nie zaczyna się od {1}.',
-    'assert_element_is_visible' : 'Element {0} jest niewidoczny.',
-    'assert_element_is_checked' : 'Element {0} nie jest zaznaczony.',
-    'assert_element_is_not_checked': 'Element {0} jest zaznaczony.',
-    'assert_element_css_attribute_equal': 'Oczekiwana wartość atrybutu {0} pola {1} to {2}. (Aktualna wartość to: {3}).',
-    'assert_element_value_is_date' : 'Oczekujemy iż wartość elementu {0} będzie datą. (Wartość elementu to: {1}).',
+    'assert_element_text_equal' : 'Oczekujemy, iż treścią elementu {0} będzie {1}. (Aktualna treść to: {2}).',
+    'assert_element_value_equal' : 'Oczekujemy, iż wartością elementu {0} będzie {1}. (Aktualna wartość to: {2}).',
+    'assert_element_value_contains' : 'Oczekujemy, iż wartość elementu {0}« będzie zawierała {1}. (Aktualna wartość to: {2}).',
+    'assert_element_value_starts' : 'Oczekujemy, iż wartość elementu {0} będzie zaczynała się od {1}. (Aktualna wartość to: {2}).',
+    'assert_element_is_visible' : 'Oczekujemy, iż element {0} będzie widoczny.',
+    'assert_element_is_checked' : 'Oczekujemy, iż element {0} będzie zaznaczony.',
+    'assert_element_is_not_checked': 'Oczekujemy, iż element {0} będzie niezaznaczony.',
+    'assert_element_css_attribute_equal': 'Oczekujemy, iż wartość atrybutu {0} pola {1} to {2}. (Aktualna wartość to: {3}).',
+    'assert_element_value_is_date' : 'Oczekujemy, iż wartość elementu {0} będzie datą. (Aktualna wartość to: {1}).',
 }
 
 def _default_assertion_message(method_name):
@@ -40,12 +40,12 @@ class SeleniumAssertionBasic:
     def assert_element_value_contains(self, element, contained_value, msg=None):
         actual_value = element.get_value()
         if contained_value not in actual_value:
-            assertion_message("assert_element_value_contains", element, contained_value, msg=msg)
+            assertion_message("assert_element_value_contains", element, contained_value, actual_value, msg=msg)
 
     def assert_element_value_starts(self, element, excepted_value, msg=None):
         actual_value = element.get_value()
         if actual_value.startswith(excepted_value) == False:
-            assertion_message("assert_element_value_starts", element, excepted_value, msg=msg)
+            assertion_message("assert_element_value_starts", element, excepted_value, actual_value, msg=msg)
 
     def assert_element_is_visible(self, element, msg=None):
         if not element.is_visible():
